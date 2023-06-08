1953f8b473ec90f18feac02e97e549bff1afc42d
Shuichi TANAKA <shu.at.kantaro.org@gmail.com>
2023-06-08 17:36:22 +0900

    eventを使ってどうにか

--- resources/js/Components/TestDiv.vue
+++ resources/js/Components/TestDiv.vue
@@ -1,8 +1,23 @@
 <template>
-    <div>
-        TEST VUE
+    <div class="testdiv">
+        <p>これはテストのビューです。どうぞよろしくお願いいたします。非常に長くなってしまった場合は折り返しますか？</p>
+        <w-button class="ma1" bg-color="secondary" xs @click="onClick">Button</w-button>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+
+
+const event = new CustomEvent('menu',{bubbles:true, detail:{ message: 'hello'}})
+
+const onClick = () => {
+    console.log('onClick')
+    document.querySelector('.testdiv')?.dispatchEvent(event)
+}
+
 </script>
+
+<style scoped></style>
