e1ea512554312ea6c651dcb09926551e9feb8137
Shuichi TANAKA <shu.at.kantaro.org@gmail.com>
2023-07-10 17:56:34 +0900

    標高APIいい感じ

--- app/Http/Controllers/Api/DemController.php
+++ app/Http/Controllers/Api/DemController.php
@@ -7,6 +7,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
 use Ramsey\Uuid\Type\Integer;
 use Illuminate\Support\Facades\Storage;
@@ -14,13 +15,9 @@ use Illuminate\Support\Facades\Storage;
 class DemController extends Controller
 {
 
-    private $dem = [
-        ['name' => '5a', 'zoom' => 15, 'url' => "https://cyberjapandata.gsi.go.jp/xyz/dem5a_png/{z}/{x}/{y}.png"],
-        ['name' => '5b', 'zoom' => 15, 'url' => "https://cyberjapandata.gsi.go.jp/xyz/dem5b_png/{z}/{x}/{y}.png"],
-        ['name' => '5c', 'zoom' => 15, 'url' => "https://cyberjapandata.gsi.go.jp/xyz/dem5c_png/{z}/{x}/{y}.png"],
-        ['name' => '10b', 'zoom' => 14, 'url' => "https://cyberjapandata.gsi.go.jp/xyz/dem_png/{z}/{x}/{y}.png"],
-        ['name' => 'aster', 'zoom' => 12, 'url' => "https://tiles.gsj.jp/tiles/elev/astergdemv3/{z}/{y}/{x}.png"],
-    ];
+    public function altitude ( Request $request ){
+
+    }
 
     private function deg2rad($deg)
     {
@@ -37,36 +34,69 @@ class DemController extends Controller
         return str_replace(["{x}", "{y}", "{z}"], [$x, $y, $z], $str);
     }
 
-    public function getAlt()
+    public function test()
     {
-        return self::getDemTile(34.38387264971507, 136.12590872583758, 14);
+        return self::getAlt(27.988015973527055,86.9251820047539);
     }
 
-    public function getDemTile(float $latDeg, float $lngDeg, $zoom = 15)
+    public function getAlt($lat, $lng)
     {
-
-        $coord = self::tilePixel($latDeg, $lngDeg, $zoom);
-        $x = $coord['tile_x'];
-        $y = $coord['tile_y'];
-        $file = sprintf("%d/%d/%d.png", $zoom, $x, $y);
-        $tile = "dem/" . $file;
-        if (!Storage::exists($tile)) {
-            $url = "https://cyberjapandata.gsi.go.jp/xyz/dem_png/" . $file;
-            //$url = "https://tiles.gsj.jp/tiles/elev/astergdemv3/" . $file;
-            $png = file_get_contents($url);
-            Storage::put($tile, $png);
+        $dem_source = [
+            ['name' => '5a', 'zoom' => 15, 'url' => "https://cyberjapandata.gsi.go.jp/xyz/dem5a_png/{z}/{x}/{y}.png"],
+            ['name' => '5b', 'zoom' => 15, 'url' => "https://cyberjapandata.gsi.go.jp/xyz/dem5b_png/{z}/{x}/{y}.png"],
+            ['name' => '5c', 'zoom' => 15, 'url' => "https://cyberjapandata.gsi.go.jp/xyz/dem5c_png/{z}/{x}/{y}.png"],
+            ['name' => '10b', 'zoom' => 14, 'url' => "https://cyberjapandata.gsi.go.jp/xyz/dem_png/{z}/{x}/{y}.png"],
+            ['name' => 'aster', 'zoom' => 12, 'url' => "https://tiles.gsj.jp/tiles/elev/astergdemv3/{z}/{y}/{x}.png"],
+        ];
+        foreach ($dem_source as $dem) {
+            $coord = self::tilePixel($lat, $lng, $dem['zoom']);
+            try {
+                $png_file = self::getTilePath($dem['url'], $dem['name'], $coord['tile_x'], $coord['tile_y'], $dem['zoom']);
+                $png = imagecreatefrompng($png_file);
+                $rgb = imagecolorat($png, $coord['pixel_x'], $coord['pixel_y']);
+                $r = ($rgb >> 16) & 0xFF;
+                $g = ($rgb >> 8) & 0xFF;
+                $b = $rgb & 0xFF;
+                if ($r === 128 && $g === 0 && $b === 0) {   // ファイルはあるが、標高値が無効
+                    continue;
+                }
+                return ['alt' => ($r * 256 * 256 + $g * 256 + $b) * 0.01, 'dem' => $dem['name']];
+            } catch (Exception $e) {
+                continue;
+            }
         }
-        $path = Storage::path($tile);
-        $png = imagecreatefrompng($path);
-        $rgb = imagecolorat($png, $coord['pixel_x'], $coord['pixel_y']);
-        $r = ($rgb >> 16) & 0xFF;
-        $g = ($rgb >> 8) & 0xFF;
-        $b = $rgb & 0xFF;
-        $alt = ($r * pow(2, 16) + $g * pow(2, 8) + $b) * 0.01;
-        return [$r, $g, $b, $alt];
+
+        return ['alt' => -9999, 'dem' => "n/a"];
     }
 
-    private function tilePixel(float $latDeg, float $lngDeg, $zoom = 15)
+
+    public function getTilePath($base_url, $name, $x, $y, $z)
+    {
+        $url = self::replace_placeholder($base_url, $x, $y, $z);
+        $local = self::replace_placeholder("dem/{z}/{x}/{y}.png." . $name, $x, $y, $z);
+        if (Storage::exists($local . ".na")) {
+            $ts = (int) trim(file_get_contents($local . ".na"));
+            if (time() > $ts + 365 * 24 * 3600) {    // 前回取得より 1年以上経過 していれば再度チャレンジしてみる
+                Storage::delete($local . ".na");
+            } else {
+                throw new \Exception('N/A');
+            }
+        }
+        if (Storage::exists($local)) {
+            return Storage::path($local);
+        }
+
+        try {
+            $png = file_get_contents($url);
+            Storage::put($local, $png);
+            return Storage::path($local);
+        } catch (Exception $e) {
+            Storage::put($local . ".na", time());
+            throw new \Exception('N/A');
+        };
+    }
+
+    private function tilePixel($latDeg,  $lngDeg, $zoom = 15)
     {
         $worldCoord = self::worldCoord($latDeg, $lngDeg, $zoom);
         $tile_x = floor($worldCoord['x'] / 256);
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('dem', 'App\Http\Controllers\Api\DemController@getAlt');
+Route::get('dem', 'App\Http\Controllers\Api\DemController@test');
