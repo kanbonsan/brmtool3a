995ac17e2bf1fe4c45378721ddb2bad120d9d2c2
Shuichi Tanaka <shu.at.kantaro.org@gmail.com>
2023-09-27 07:36:54 +0900

    sviwcontent

--- resources/js/Components/SvInfoContent.vue
+++ resources/js/Components/SvInfoContent.vue
@@ -1,6 +1,7 @@
 <template>
     <div v-if="cuePoint">
-        Hello, {{ cuePoint?.properties.name }}
+        <p style="text-transform: none;">{{ label }}</p>
+         <p>{{ name }}</p>
     </div>
 </template>
 
@@ -11,8 +12,22 @@ const props = defineProps<{
     cuePoint: CuePoint | undefined
 }>()
 const label = computed(()=>{
-    
+    const cpt = props.cuePoint
+    if(!cpt)return
+    let _label = ''
+    switch(cpt.type){
+        case 'pc':
+            _label=`PC ${cpt.controlLabel}`
+            break
+        case 'pass':
+            _label =`CHK ${cpt.controlLabel}`
+            break
+        case 'cue':
+            _label =`No ${cpt.pointNo}`
+    }
+    return _label
 })
+const name = computed(()=>props.cuePoint?.cuesheetName)
 
 </script>
 
